13205d4637718f676d91264c29d01f90e2685a9c
Dallan <dallan598@hotmail.com>
2019-06-27 21:52:17 -0600

    -m

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+            String name;
+            int roll_no;
+    int phoneNmbr;
+    String address;
+        }
+        class Sam{
+  String name;
+  int roll_no;
+  int phoneNmbr;
+  String address;
+        }
---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "john";
+        s.roll_no = 2;
+        s.address = "69 memeville";
+        s.phoneNmbr = 4004569 ;
+        Sam x = new Sam();
+        x.name = "Sam";
+        x.roll_no = 4;
+        x.address = "34.5 pepega" ;
+        x.phoneNmbr = 9001 ;
+        System.out.println("name is " + s.name + " roll is " + s.roll_no);
+        System.out.println("name is " + x.name + " roll is " + x.roll_no);
+    }
+}
